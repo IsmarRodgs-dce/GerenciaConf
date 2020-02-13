2170d46b374200e9c9916dffabde706f9600e26a
ismar.rodrigues <ismar.rodrigues@LAB2-DCX-08.labs.dcx>
2020-02-13 07:45:59 -0300

    Feito a classe.

---
+++ EngenSoft/src/Ismar.java
@@ -0,0 +1,15 @@
+
+public class Ismar {
+	public static int somar(int v1, int v2) {
+		return v1+v2;
+	}
+	
+	
+	public static void main(String[] args) {
+		int nota1 = 10, nota2 = 20, nota3;
+		nota3 = somar(nota1, nota2);
+		System.out.println(nota3);
+		
+		
+	}
+}
